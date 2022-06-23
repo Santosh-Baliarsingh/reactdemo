e66a266e8e690900ec9100a5e63a651869684a2d
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-06-23 21:56:47 +0530

    useNavigate Hook from react-router-dom Added in About.js File

--- src/Components/routing/about/About.js
+++ src/Components/routing/about/About.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function About() {
+  // To use navigate after doing some operations the  must use "useNavigate() Hook"
+  const navigate = useNavigate();
+
+  const f1 = () =>{
+
+    // Do Somthing (Operation) before Jump to Home page
+    alert("Operation Done...");
+
+    // After Done Operation then jump to Home page
+    navigate("/home");
+     
+  };
+
+  // In above Here we are navigating one route to another router before doing some operations programmatically
   return (
-    <div className='text-center text-success h3 mt-3'>This is About Page...</div>
-  )
+    <>
+      <div className="text-center text-success h3 mt-3">
+        This is About Page...
+      </div>
+      <button className="btn btn-success ms-2" onClick={f1}>CLick To Go Home Page</button>
+      {/* Here after click the button a alert message will show then navigate to the Home Page*/}
+    </>
+  );
 }

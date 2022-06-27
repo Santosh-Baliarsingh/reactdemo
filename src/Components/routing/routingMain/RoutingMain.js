3579fb2370b4f2b6375bd624e7d26e0a7ef8fc4e
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-06-27 14:42:55 +0530

    New Routes Added in RoutingMain.js File

--- src/Components/routing/routingMain/RoutingMain.js
+++ src/Components/routing/routingMain/RoutingMain.js
@@ -5,7 +5,9 @@ import ContactUs from "../contactUs/ContactUs";
 import Home from "../home/Home";
 import NotFound from "../notFound/NotFound";
 import ProductDetails from "../productDetails/ProductDetails";
+import ProductDetails2 from "../productDetails/ProductDetails2";
 import ProductList from "../productList/ProductList";
+import ProductList2 from "../productList/ProductList2";
 import RoutingNavbar from "../routingnavbar/RoutingNavbar";
 
 
@@ -19,7 +21,11 @@ export default function RoutingMain() {
         <Route exact path="/about" element={<About />} />
         <Route exact path="/contactUs" element={<ContactUs />} />
         <Route exact path="/productList" element={<ProductList />} />
+        <Route exact path="/productList2" element={<ProductList2 />} />
+        {/* Below Router is Path Params */}
         <Route exact path="/productDetails/:id" element={<ProductDetails/>} />
+        {/* Below Route is Query Params */}
+        <Route exact path="/productDetails" element={<ProductDetails2/>}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>

2399346b4a8597bbf9a1f000095d0374d008ed5b
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-06-28 12:18:30 +0530

    NestedRouting Link Added in RoutingNavbar.js File

--- src/Components/routing/routingnavbar/RoutingNavbar.js
+++ src/Components/routing/routingnavbar/RoutingNavbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import './RoutingNavBar.css'
+import "./RoutingNavBar.css";
 
 export default function RoutingNavbar() {
   let [toggle, setToggle] = useState(true); // default Value is true
@@ -12,7 +12,7 @@ export default function RoutingNavbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark ">
       <NavLink className="nav-link active fw-bold  h4" to="/">
-         React
+        React
       </NavLink>
       <button
         className="navbar-toggler me-3 bg-light"
@@ -36,23 +36,31 @@ export default function RoutingNavbar() {
             </NavLink>
           </li>
           <li className="nav-item ms-2">
-          <NavLink className="nav-link  fw-bold text-light" to="/about">
+            <NavLink className="nav-link  fw-bold text-light" to="/about">
               About
             </NavLink>
           </li>
           <li className="nav-item mx-2">
-          <NavLink className="nav-link  fw-bold text-light" to="/contactUs">
+            <NavLink className="nav-link  fw-bold text-light" to="/contactUs">
               ContactUs
             </NavLink>
           </li>
           <li className="nav-item mx-2">
-          <NavLink className="nav-link fw-bold text-light" to="/productlist">
-               ProductList
+            <NavLink className="nav-link fw-bold text-light" to="/productlist">
+              ProductList
             </NavLink>
           </li>
           <li className="nav-item mx-2">
-          <NavLink className="nav-link fw-bold text-light" to="/productlist2">
-               ProductList2
+            <NavLink className="nav-link fw-bold text-light" to="/productlist2">
+              ProductList2
+            </NavLink>
+          </li>
+          <li className="nav-item mx-2">
+            <NavLink
+              className="nav-link fw-bold text-light"
+              to="/nestedRouting"
+            >
+              NestedRouting
             </NavLink>
           </li>
         </ul>
